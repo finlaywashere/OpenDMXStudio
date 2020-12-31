3c4ba7cf91992d0ef12080d0d3a9247f2045a4b4
finlaywashere <finman292004@protonmail.com>
2020-12-31 02:02:42 -0500

    Got start of backend and frontend set up

--- src/xyz/finlaym/opendmx/OpenDMXStudio.java
+++ src/xyz/finlaym/opendmx/OpenDMXStudio.java
@@ -1,5 +1,28 @@
 package xyz.finlaym.opendmx;
 
-public class OpenDMXStudio {
-	
+import com.fazecast.jSerialComm.SerialPort;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import xyz.finlaym.opendmx.driver.HardwareInterface;
+
+public class OpenDMXStudio extends Application{
+	public static void main(String[] args) {
+		launch(args);
+	}
+	private HardwareInterface hwInterface;
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// We starting bois
+		SerialPort comPort = SerialPort.getCommPorts()[0];
+		comPort.setBaudRate(9600);
+		comPort.openPort();
+		
+		// Now we have an open port
+		hwInterface = new HardwareInterface(comPort);
+		
+	}
+	public HardwareInterface getHwInterface() {
+		return hwInterface;
+	}
 }
--- src/xyz/finlaym/opendmx/driver/HardwareInterface.java
+++ src/xyz/finlaym/opendmx/driver/HardwareInterface.java
@@ -1,10 +1,23 @@
 package xyz.finlaym.opendmx.driver;
 
+import java.io.IOException;
+
+import com.fazecast.jSerialComm.SerialPort;
+
 import xyz.finlaym.opendmx.command.Command;
 
 public class HardwareInterface {
-	public static void sendCommand(Command c) {
+	private SerialPort serial;
+	
+	public HardwareInterface(SerialPort serial) {
+		this.serial = serial;
+	}
+	public SerialPort getSerial() {
+		return serial;
+	}
+	public void sendCommand(Command c) throws IOException {
 		byte[] data = c.encode();
-		
+		serial.getOutputStream().write(data);
+		serial.getOutputStream().flush();
 	}
 }
