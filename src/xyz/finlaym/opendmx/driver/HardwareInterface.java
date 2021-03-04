5687ae60f4986f47c35f510dbcb4179f9bc2c5ca
finlaywashere <finman292004@protonmail.com>
2021-03-04 15:07:38 -0500

    Added copyright notice to all source files

--- src/xyz/finlaym/opendmx/driver/HardwareInterface.java
+++ src/xyz/finlaym/opendmx/driver/HardwareInterface.java
@@ -1,3 +1,17 @@
+/*
+    Copyright (C) 2021 Finlay Maroney
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package xyz.finlaym.opendmx.driver;
 
 import java.io.IOException;
@@ -16,7 +30,6 @@ public class HardwareInterface {
 		return serial;
 	}
 	public void sendCommand(Command c) throws IOException {
-		System.out.println("dmx_send");
 		byte[] data = c.encode();
 		if(!serial.isOpen()) {
 			System.err.println("Error: Port is not open!");
